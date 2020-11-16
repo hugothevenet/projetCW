9df0bddce34373a328e547f1dd3a9da66efd91ed
Thevenet Hugo <hugo.thevenet777@gmail.com>
2020-11-16 12:25:27 +0100

    test de push2

--- app/src/main/java/com/example/projetcw/MainActivity.java
+++ app/src/main/java/com/example/projetcw/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //deuxième test
         // gros test je test
     }
 }
